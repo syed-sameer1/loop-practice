fb9f67d9ebd77a533614bfed0008fa4f26814741
syed-sameer1 <48875785+syed-sameer1@users.noreply.github.com>
2019-06-25 12:09:31 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,136 @@
+
+function onClick() {
+    let start = Number(document.querySelector("#start").value);
+    let end = Number(document.querySelector("#end").value);
+    if (start > end) {
+        for (let i = start; i >= end; i--) {
+            console.log(i);
+        }
+    } else{
+        for (let i = start; i <= end; i++) {
+            console.log(i);
+        }
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// - kia chalana hay
+// - kitni bar chalana hay
+
+
+
+
+
+
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
+// console.log("clap!");
